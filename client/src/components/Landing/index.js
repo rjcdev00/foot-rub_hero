2e9a6b04c3eed269d9b96c56b6d3bd3ddd8ce1bb
Meanderthal00 <meanderthal00@yahoo.com>
2018-03-31 07:26:15 -0400

    pages started

---
+++ client/src/components/Landing/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./style.css";
+
+const Landing = (props) =>{
+	return (
+        
+
+		<div>
+			<h1>You should only see this if you are logged in!</h1>
+			<p>{props.auth.username}</p>
+			<button onClick = {props.handleLogout}>Log Out</button>
+		</div>
+	);
+}
+
+export default Home;
