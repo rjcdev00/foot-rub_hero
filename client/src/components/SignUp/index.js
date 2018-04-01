35c7efb6f3641505b08315f44e3c7db1e2ebab49
Meanderthal00 <meanderthal00@yahoo.com>
2018-04-01 15:50:17 -0400

    trouble every-day

--- client/src/components/SignUp/index.js
+++ client/src/components/SignUp/index.js
@@ -1,23 +1,45 @@
 import React from 'react';
-import "./style.css";
 import {Link} from 'react-router-dom';
+import {Card, Button, Row, Col} from 'react-materialize';
+
+import "./style.css";
 
 const SignUp = (props)=> {
+
 	return (
-		<div>
-			<h1>SIGN UP</h1>
-			<Link to = "/" >Go to sign in</Link>
-			<form>
-				<label>Email</label><br/>
-				<input value = {props.username} onChange = {props.handleChange} name='username' type='email' placeholder = 'example@email.com'/>
-				<br />
-				<label>Password</label><br/>
-				<input name='password' type='password' value = {props.password} onChange = {props.handleChange} />
-				<br />
-				<button type = 'submit' name = "/auth/signup" onClick = {props.handleSubmit}>Sign Up</button>
-			</form>
-		</div>
+		<div className="container" id="signUpCont">
+		 
+
+
+		<Row>				
+		<Col m={4} offset={'m4'} className='signUp-card'>
+	<Card className='blue-grey darken-2' textClassName='white-text' title='SIGN UP'actions={[<Link to = "/" >Go to sign in</Link>]} >	
+	
+	
+		<form>
+			<label>Email</label><br/>
+			<input value = {props.username} onChange = {props.handleChange} name='username' type='email' placeholder = 'example@email.com'/>
+			<br />
+			<label>Password</label><br/>
+			<input name='password' type='password' value = {props.password} onChange = {props.handleChange} />
+			<br />
+			{/* <button type = 'submit' name = "/auth/signin" onClick = {props.handleSubmit}>Sign In</button> */}
+			<Button className="btn waves-effect waves-light" type="submit" name="/auth/signup"  onClick = {props.handleSubmit}>Sign up 
+	   <i className="material-icons right">send</i>
+		  </Button>
+		</form>
+		
+	
+	</Card>
+	</Col>
+	</Row>
+
+	</div>
 	);
+
 }
+
+
+
 
 export default SignUp;
